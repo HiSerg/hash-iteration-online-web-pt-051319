ae1ab70cf7252d42d71082f40f7798e04c4625d8
Sergio Guzman <hiserg98@gmail.com>
2019-06-18 23:50:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -25,4 +25,5 @@ def age_apprpriate_birthday(birthday_kids)
   end 
   younger_birthday.each do |kids_name, age|
     puts ("Happy Birthday #{kids_name}! You are now #{age} years old!")
+  end
 end 
