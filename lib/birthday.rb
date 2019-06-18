664ee301e04b5fef10610b3d623ab32779f7600d
Sergio Guzman <hiserg98@gmail.com>
2019-06-18 23:49:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -19,7 +19,10 @@ def age_apprpriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
       younger_birthday[kids_name] = {age}
+    else
+      
     end
   end 
-  younger_birthday
+  younger_birthday.each do |kids_name, age|
+    
 end 
