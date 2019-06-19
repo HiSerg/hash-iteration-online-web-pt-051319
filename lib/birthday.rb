94c897860cbc219054c1b457a6fa0aae95ca2b03
Sergio Guzman <hiserg98@gmail.com>
2019-06-19 00:04:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,16 +5,12 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
-  
-  birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27}
   birthday_kids.each do |kids_name, age|
     puts ("Happy Birthday #{kids_name}! You are now #{age} years old!")
   end
 end 
 
 def age_apprpriate_birthday(birthday_kids)
- birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27}
  younger_birthday = {}
   birthday_kids.each do |kids_name, age|
     if age <= 12
