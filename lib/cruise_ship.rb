526b843641d124c3a2b0f979f1809f1322b41c16
Sergio Guzman <hiserg98@gmail.com>
2019-06-19 00:00:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,10 @@
 # }
 
 def select_winner(passengers)
-  passengers = {
+  passengers = { "suite_a": "Amanda Presley", 
+ "suite_b": "Seymour Hoffman", 
+ "suite_c": "Alfred Tennyson", 
+ "suite_d": "Charlie Chaplin", 
+"suite_e": "Crumpet the Elf" }
+
 end
